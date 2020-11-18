892d5b9b7974564c0950b996ebbc0c04ce398c96
iggi-71 <jimenez72iggi@gmail.com>
2020-11-17 19:04:30 -0800

    first problem done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//var colorHex: [String:String] = [
+//    "purple" : "#6C3483",
+//    "blue" : "#2874A6",
+//    "green": "#1E8449" ,
+//    "orange" : "#D35400",
+//    "yellow" : "#F1C40F",
+//    "white" : "#FFFFFF",
+//    "black" : "#000000"
+//]
+//
+//for (key, value) in colorHex {
+//   print("\(value)")
+//}
+
+var colors = ["red", "orange", "purple", "pink", "blue", "red", "green", "red", "blue", "purple", "pink" , "purple", "purple"]
+
