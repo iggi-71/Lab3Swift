8eeb29315edb7d4b8a45f363eaeafb945ba6371c
iggi-71 <jimenez72iggi@gmail.com>
2020-11-17 20:35:12 -0800

    second question is completed

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,19 +2,61 @@ import UIKit
 
 var str = "Hello, playground"
 
-//var colorHex: [String:String] = [
-//    "purple" : "#6C3483",
-//    "blue" : "#2874A6",
-//    "green": "#1E8449" ,
-//    "orange" : "#D35400",
-//    "yellow" : "#F1C40F",
-//    "white" : "#FFFFFF",
-//    "black" : "#000000"
-//]
-//
-//for (key, value) in colorHex {
-//   print("\(value)")
-//}
+print("-----------------------------------------------------")
+print("Question Number 1")
+print("")
+
+var colorHex: [String:String] = [
+    "purple" : "#6C3483",
+    "blue" : "#2874A6",
+    "green": "#1E8449" ,
+    "orange" : "#D35400",
+    "yellow" : "#F1C40F",
+    "white" : "#FFFFFF",
+    "black" : "#000000"
+]
+
+for (key, value) in colorHex {
+   print("\(value)")
+}
+
+print("")
+print("-----------------------------------------------------")
+print("Question Number 2")
+print("")
+
 
 var colors = ["red", "orange", "purple", "pink", "blue", "red", "green", "red", "blue", "purple", "pink" , "purple", "purple"]
+ 
+var red = 0
+var orange = 0
+var purple = 0
+var pink = 0
+var blue = 0
+var green = 0
 
+for color in colors {
+    if color == "red"{
+        red += 1
+    } else if color == "orange"{
+        orange += 1
+    } else if color == "purple"{
+        purple += 1
+    } else if color == "pink"{
+        pink += 1
+    } else if color == "blue"{
+        blue += 1
+    } else {
+        green += 1
+    }
+}
+
+print("Red printed this number of times:", red)
+print("Blue printed this number of times:", blue)
+print("Orange printed this number of times:", orange)
+print("Purple printed this number of times:", purple)
+print("Pink printed this number of times:", pink)
+print("Green printed this number of times:", green)
+
+print("")
+print("-----------------------------------------------------")
